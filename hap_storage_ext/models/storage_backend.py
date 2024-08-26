63d5f30dc86bed80c044c83027c059face29834a
Umer Ashraf <umer@knkit.com>
2024-08-27 01:00:10 +0800

    Storage File to IR Attachment removal

--- hap_storage_ext/models/storage_backend.py
+++ hap_storage_ext/models/storage_backend.py
@@ -4,3 +4,13 @@ class storageBackend(models.Model):
     _inherit = "storage.backend"
 
     res_model = fields.Char(string="For Model", required=True)
+
+class StorageFile(models.Model):
+    _inherit = "storage.file"
+
+    def unlink(self):
+        attachment = self.env["ir.attachment"].search([("storage","=",self.id)])
+        if attachment:
+            attachment.unlink()
+        super(StorageFile, self).unlink()
+        return True
